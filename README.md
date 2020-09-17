ddb2a6516b3f582efe44f67fe7dcee37b01fbe11
Dustin Davis <dustin.davis@verizonmedia.com>
2020-09-17 13:33:14 -0600

    Update to work with PHP 7+
    + Add ability to set a custom short-link
    + Update README

--- README.md
+++ README.md
@@ -13,4 +13,25 @@ Goals of this script:
 - SQLite for storage
 - API for use with Tweetbot
 
+## Requirements
+
+- PHP 7+
+- SQLite3+
+
+## How it works
+
+This is very simple. There are a few GET parameters you can pass in.
+
+### `?u=https://example.com`
+
+This will create a new short link and return the short-link URL
+
+### `?u=https://example.com&custom=example`
+
+Similar to above, but this will allow you to set a custom short-link. If the custom short-link is in use, you will get a 409 error code.
+
+### `?list`
+
+This will list all the available short-links available in a simple table.
+
 See blog post: https://dustindavis.me/blog/phurlite-simple-php-sqlite-url-shortener
--- index.php
+++ index.php
@@ -1,96 +1,121 @@
 <?php
 
-define('DBNAME', 'phurlite.sqlite3');
+define('DBNAME', 'phurlite.sqlite3.db');
 define('SHORT_STR_LEN', 4);
-define('SHORT_CHARS', '0123456789abcdefghijklmnopqrstvwxyzABCDEFGHIJKLMNOPQRSTVWXYZ'); 
+define('SHORT_CHARS', '0123456789abcdefghijklmnopqrstvwxyzABCDEFGHIJKLMNOPQRSTVWXYZ');
 define('HOST', $_SERVER['HTTP_HOST']);
 
 $create_table = !file_exists(DBNAME);
 
-if ($db = sqlite_open(DBNAME, 0666, $sqlite_error)) {
-	if ($create_table) {
-		$query = '
-			CREATE TABLE "links" (
-			"url" TEXT NOT NULL ON CONFLICT IGNORE UNIQUE ON CONFLICT IGNORE,
-			"short" TEXT UNIQUE,
-			"hits" INTEGER DEFAULT 0
-			);
-		';
-		sqlite_query($db, $query);
-	}
-}
-else {
-	die($sqlite_error);
+$db = new SQLite3(DBNAME);
+if ($create_table) {
+    $query = '
+        CREATE TABLE "links" (
+        "url" TEXT NOT NULL ON CONFLICT IGNORE,
+        "short" TEXT UNIQUE,
+        "hits" INTEGER DEFAULT 0
+        );
+    ';
+    $db->exec($query);
 }
 
-if ($_GET['u']) {
-	$short_str = add_url($_GET['u'], $db);
-	header('Content-type: text/plain');
-	echo shortlink($short_str);
-	exit();
+if (isset($_GET['u'])) {
+    $custom = isset($_GET['custom']) ? $_GET['custom'] : false;
+    $short_str = add_url($_GET['u'], $custom, $db);
+    header('Content-type: text/plain');
+    echo shortlink($short_str);
+    exit();
 }
 
-if ($_GET['p']) {
-	$url = get_url($_GET['p'], $db);
-	add_hit($url, $db);
-	header("Location: $url");
-	exit();
+if (isset($_GET['p'])) {
+    $url = get_url($_GET['p'], $db);
+    add_hit($url, $db);
+    header("Location: $url");
+    exit();
 }
 
 if (isset($_GET['list'])) {
-	$query = "SELECT * FROM links ORDER BY rowid DESC;";
-	$res = sqlite_query($db, $query);
-	echo '<table><tr><th>Shortened</th><th>Original</th><th>Hits</th></tr>';
-	while ($row = sqlite_fetch_object($res)) {
-		echo '<tr><td>'.shortlink_html($row->short)."</td><td>$row->url</td><td>$row->hits</td></tr>";
-	}
-	echo '</table>';
+    $query = 'SELECT * FROM links ORDER BY rowid DESC;';
+    $res = $db->query($query);
+    echo '<table><tr><th>Shortened</th><th>Original</th><th>Hits</th></tr>';
+    while ($row = $res->fetchArray()) {
+        echo '<tr><td>' . shortlink_html($row['short']) . "</td><td>{$row['url']}</td><td>{$row['hits']}</td></tr>";
+    }
+    echo '</table>';
 }
 
-function add_url($url, &$db) {
-	# verify it is unique
-	$query = "SELECT short FROM links WHERE url='$url';";
-	$res = sqlite_query($db, $query);
-	if (sqlite_num_rows($res)) {
-		$row = sqlite_fetch_array($res);
-		return $row['short'];
-	}
-	else {
-		$short = get_short_str($url, $db);
-		$query = "INSERT INTO links (url, short) VALUES ('$url', '$short');";
-		sqlite_query($db, $query);
-		return $short;
-	}
+function add_url($url, $custom, &$db)
+{
+    if ($custom) {
+        # make sure custom URL is not in use
+        $stmt = $db->prepare('SELECT short, url FROM links WHERE short = :short;');
+        $stmt->bindParam(':short', $custom, SQLITE3_TEXT);
+        $res = $stmt->execute();
+        $row = $res->fetchArray();
+        if ($row) {
+            if ($row['url'] == $url) {
+                return $custom;
+            }
+            http_response_code(409);
+            echo 'This custom short code is already in use.';
+            exit;
+        }
+        $short = $custom;
+    } else {
+        # verify it is unique
+        $statement = $db->prepare('SELECT short FROM links WHERE url = :url');
+        $statement->bindParam(':url', $url, SQLITE3_TEXT);
+        $res = $statement->execute();
+        $row = $res->fetchArray();
+        if ($row) {
+            return $row['short'];
+        } else {
+            $short = get_short_str($url, $db);
+        }
+    }
+    $statement = $db->prepare('INSERT INTO links (url, short) VALUES (:url, :short);');
+    $statement->bindParam(':url', $url, SQLITE3_TEXT);
+    $statement->bindParam(':short', $short, SQLITE3_TEXT);
+    $statement->execute();
+    return $short;
 }
 
-function get_short_str($url, &$db) {
-	$short = substr(str_shuffle(SHORT_CHARS), 0, SHORT_STR_LEN);
-	# make sure it is not being used
-	$query = "SELECT short FROM links WHERE short='$short';";
-	$res = sqlite_query($db, $query);
-	if (sqlite_num_rows($res)) {
-		return get_short_str($url, $db);
-	}
-	return $short;
+function get_short_str($url, &$db)
+{
+    $short = substr(str_shuffle(SHORT_CHARS), 0, SHORT_STR_LEN);
+    # make sure it is not being used
+    $statement = $db->prepare('SELECT short FROM links WHERE short = :short;');
+    $statement->bindParam(':short', $short, SQLITE3_TEXT);
+    $res = $statement->execute();
+    if ($res->fetchArray()) {
+        return get_short_str($url, $db);
+    }
+    return $short;
 }
 
-function get_url($short_str, &$db) {
-	$query = "SELECT url FROM links WHERE short='$short_str';";
-	$res = sqlite_query($db, $query);
-	$row = sqlite_fetch_object($res);
-	return $row->url;
+function get_url($short_str, &$db)
+{
+    $statement = $db->prepare('SELECT url FROM links WHERE short = :short;');
+    $statement->bindParam(':short', $short_str);
+    $res = $statement->execute();
+    $row = $res->fetchArray();
+    return $row['url'];
 }
 
-function add_hit($url, &$db) {
-	$query = "UPDATE links SET hits = hits + 1 WHERE url='$url';";
-	sqlite_query($db, $query);
+function add_hit($url, &$db)
+{
+    $statement = $db->prepare('UPDATE links SET hits = hits + 1 WHERE url = :url;');
+    $statement->bindParam(':url', $url, SQLITE3_TEXT);
+    $statement->execute();
 }
 
-function shortlink($short_str) {
-	return 'http://'.HOST.'/'.$short_str;
+function shortlink($short_str)
+{
+    return 'http://' . HOST . '/' . $short_str;
 }
 
-function shortlink_html($short_str) {
-	$shortlink = shortlink($short_str);
-	return "<a href=\"$shortlink\">$short_str</a>";
+function shortlink_html($short_str)
+{
+    $shortlink = shortlink($short_str);
+    return "<a href=\"$shortlink\">$short_str</a>";
 }
